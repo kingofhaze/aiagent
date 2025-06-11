61f6a876000a124da39a0059a81e953c4eb22421
kingofhaze <194982118+kingofhaze@users.noreply.github.com>
2025-06-11 03:02:23 -0400

    wip: C2L3

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,14 +1,14 @@
 import os
 
 def get_files_info(working_directory, directory=None):
-    if not os.path.abspath(os.path.join(working_directory, directory or '')).startswith(os.path.abspath(working_directory)):
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-
-    if not os.path.isdir(os.path.abspath(os.path.join(working_directory, directory or ''))):
-        return f'Error: "{directory}" is not a directory'
-    
-    files_info = []
     try:
+        if not os.path.abspath(os.path.join(working_directory, directory or '')).startswith(os.path.abspath(working_directory)):
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+        if not os.path.isdir(os.path.abspath(os.path.join(working_directory, directory or ''))):
+            return f'Error: "{directory}" is not a directory'
+        
+        files_info = []
         contents = os.listdir(os.path.join(working_directory, directory or ''))
         
         for item in contents:
@@ -18,6 +18,25 @@ def get_files_info(working_directory, directory=None):
 
             files_info.append(f"- {item}: file_size={fileSize} bytes, is_dir={isDir}")
         return "\n".join(files_info)
+    except Exception as e:
+        return f'Error: {str(e)}'
+    
+def get_file_content(working_directory, file_path):
+    try:
+        if not os.path.abspath(os.path.join(working_directory, file_path)).startswith(os.path.abspath(working_directory)):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+        if not os.path.exists(os.path.join(working_directory, file_path)) or not os.path.isfile(os.path.join(working_directory, file_path)):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+        with open(os.path.join(working_directory, file_path), 'r') as file:
+            content = file.read()
+
+            if len(content) > 10000:
+                return f'{content[:10000]}[...File "{file_path}" truncated at 10000 characters]'            
+            
+            return content
+
     except Exception as e:
         return f'Error: {str(e)}'
     
--- tests.py
+++ tests.py
@@ -1,6 +1,5 @@
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_file_content
 
-print(f"{get_files_info("calculator", ".")}")
-print(f"{get_files_info("calculator", "pkg")}")
-print(f"{get_files_info("calculator", "/bin")}")
-print(f"{get_files_info("calculator", "../")}")
+print(f'{get_file_content("calculator", "main.py")}')
+print(f'{get_file_content("calculator", "pkg/calculator.py")}')
+print(f'{get_file_content("calculator", "/bin/cat")}')
