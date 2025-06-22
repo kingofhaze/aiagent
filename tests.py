cff64a919078d09acbdcfab960dd70a817d4c05a
kingofhaze <194982118+kingofhaze@users.noreply.github.com>
2025-06-22 02:03:34 -0400

    C2L5

--- tests.py
+++ tests.py
@@ -1,12 +1,13 @@
-from functions.write_file import write_file
-from functions.get_file_content import get_file_content
+from functions.run_python import run_python_file
 
 def main():
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    result = run_python_file("calculator", "main.py")
     print(result)
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    result = run_python_file("calculator", "tests.py")
     print(result)
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    result = run_python_file("calculator", "../main.py")
+    print(result)
+    result = run_python_file("calculator", "nonexistent.py")
     print(result)
 
 if __name__ == "__main__":
