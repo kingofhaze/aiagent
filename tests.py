0eac02ec221f2513a6a5513012db029279cbf33b
kingofhaze <194982118+kingofhaze@users.noreply.github.com>
2025-06-22 01:31:25 -0400

    C2L4

--- tests.py
+++ tests.py
@@ -1,15 +1,13 @@
+from functions.write_file import write_file
 from functions.get_file_content import get_file_content
 
-def test():
-    result = get_file_content("calculator", "main.py")
+def main():
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
     print(result)
-
-    result = get_file_content("calculator", "pkg/calculator.py")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
     print(result)
-
-    result = get_file_content("calculator", "/bin/cat")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
     print(result)
-
 
 if __name__ == "__main__":
-    test()
+    main()
